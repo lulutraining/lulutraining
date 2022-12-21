9d79aea88a15b63629ef1101942f618ff0f45d56
박지윤 <jyun1937@naver.com>
2022-12-21 23:24:43 +0900

    Fix: firebase env key 변경

--- services/firebase.ts
+++ services/firebase.ts
@@ -2,9 +2,9 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
