eba9b188b106028f72ac5eea950487f505297a6c
박지윤 <jyun1937@naver.com>
2023-01-29 23:09:24 +0900

    Style: 로그인 인증 체크 모달 theme & 로고 수정

--- components/UnathorizedResult/index.tsx
+++ components/UnathorizedResult/index.tsx
@@ -1,5 +1,5 @@
 import { Container } from './style';
-import logoImage from '/public/images/logo.png';
+import symbol from '/public/images/symbol.png';
 import { Box, Button } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -12,7 +12,7 @@ export const UnathorizedResult = ({ isLogin }: UnathorizedResultProps) => {
   return (
     <Container open={isLogin}>
       <Box>
-        <Image src={logoImage} alt="oz-logo" priority />
+        <Image src={symbol} alt="lulutraining-symbol" priority />
         <p>로그인이 필요한 서비스 입니다</p>
         <Button type="button">
           <Link href="/signin">로그인</Link>
