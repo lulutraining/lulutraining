1d028fed7f54cc8d5c1316ead031f07712cd7cfe
박지윤 <jyun1937@naver.com>
2023-02-07 17:56:47 +0900

    Fix: splash 컴포넌트 삭제

--- pages/signup/[check].tsx
+++ pages/signup/[check].tsx
@@ -1,4 +1,3 @@
-import { Splash } from 'components';
 import { Container } from 'styles/signupCheck.style';
 import logoImage from 'public/images/logo.png';
 import { Button } from '@mui/material';
@@ -10,16 +9,14 @@ import { useEffect, useState } from 'react';
 const CheckIsWritePersonalInfo = () => {
   const router = useRouter();
   const { check } = router.query;
-  const [isComplete, setIsComplete] = useState(false);
   const [isBodyCheck, setIsBodyCheck] = useState(true);
 
   const onMoveNext = () => {
     isBodyCheck ? router.push('/bodycheck') : router.push('/activecheck');
   };
   const onSkipCheck = () => {
-    isBodyCheck ? router.push('/signup/active-check') : setIsComplete(true);
+    isBodyCheck ? router.push('/signup/active-check') : router.push('/');
   };
-
   useEffect(() => {
     if (check === 'body-check') {
       setIsBodyCheck(true);
@@ -30,36 +27,30 @@ const CheckIsWritePersonalInfo = () => {
 
   return (
     <Container>
-      {isComplete ? (
-        <Splash />
-      ) : (
-        <>
-          <Image src={logoImage} alt="lulutraining-image" />
-          <div className="check__guide-text">
-            {check === 'body-check' ? (
-              <>
-                <p>신체정보를 기입해주시면</p>
-                <p>회원님의 기초대사량을</p>
-                <p>측정해드립니다</p>
-              </>
-            ) : (
-              <>
-                <p>활동량을 알려주시면</p>
-                <p>회원님께 알맞은 운동 코스를</p>
-                <p>추천해드립니다</p>
-              </>
-            )}
-          </div>
-          <div className="check__button">
-            <Button type="button" onClick={onMoveNext}>
-              입력하기
-            </Button>
-            <Button type="button" onClick={onSkipCheck}>
-              건너뛰기
-            </Button>
-          </div>
-        </>
-      )}
+      <Image src={logoImage} alt="lulutraining-image" />
+      <div className="check__guide-text">
+        {check === 'body-check' ? (
+          <>
+            <p>신체정보를 기입해주시면</p>
+            <p>회원님의 기초대사량을</p>
+            <p>측정해드립니다</p>
+          </>
+        ) : (
+          <>
+            <p>활동량을 알려주시면</p>
+            <p>회원님께 알맞은 운동 코스를</p>
+            <p>추천해드립니다</p>
+          </>
+        )}
+      </div>
+      <div className="check__button">
+        <Button type="button" onClick={onMoveNext}>
+          입력하기
+        </Button>
+        <Button type="button" onClick={onSkipCheck}>
+          건너뛰기
+        </Button>
+      </div>
     </Container>
   );
 };
@@ -72,6 +63,7 @@ export const getServerSideProps = async ({ params }: GetServerSidePropsContext) 
       notFound: true,
     };
   }
+
   return {
     props: {},
   };
