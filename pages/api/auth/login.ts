2b848304d62f3a976d5eb366d97e896fb448642c
박지윤 <jyun1937@naver.com>
2023-02-09 17:24:17 +0900

    Fix: 토큰 인증 코드 추가

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -5,16 +5,22 @@ import { adminAuth } from 'services/admin';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { token } = req.body;
   if (token) {
-    const getSessionCookie = await adminAuth.createSessionCookie(token, {
-      expiresIn: 336 * 60 * 60 * 1000,
-    });
-    const userToken = serialize('user', getSessionCookie, {
-      httpOnly: true,
-      path: '/',
-      expires: new Date(Date.now() + 336 * 60 * 60 * 1000),
-    });
-
-    res.setHeader('Set-Cookie', userToken);
+    await adminAuth
+      .verifyIdToken(token)
+      .then(async () => {
+        const getSessionCookie = await adminAuth.createSessionCookie(token, {
+          expiresIn: 336 * 60 * 60 * 1000,
+        });
+        const userToken = serialize('user', getSessionCookie, {
+          httpOnly: true,
+          path: '/',
+          expires: new Date(Date.now() + 336 * 60 * 60 * 1000),
+        });
+        res.setHeader('Set-Cookie', userToken);
+      })
+      .catch(() => {
+        res.status(401).json({ message: 'invlalid token' });
+      });
   }
   res.status(200).json({ message: 'Successfully set cookie!' });
 };
