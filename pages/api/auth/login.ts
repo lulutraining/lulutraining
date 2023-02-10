59ca55f1c7faa152de16fbac06df29c2709ee410
박지윤 <jyun1937@naver.com>
2023-02-10 10:11:45 +0900

    Fix: 로그인 api try catch 구문으로 수정

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -4,23 +4,28 @@ import { adminAuth } from 'services/admin';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { token } = req.body;
+  const verifyToken = async (token: string) => {
+    return await adminAuth.verifyIdToken(token, true);
+  };
+  const createCookie = async (token: string) => {
+    return await adminAuth.createSessionCookie(token, {
+      expiresIn: 336 * 60 * 60 * 1000,
+    });
+  };
+
   if (token) {
-    await adminAuth
-      .verifyIdToken(token)
-      .then(async () => {
-        const getSessionCookie = await adminAuth.createSessionCookie(token, {
-          expiresIn: 336 * 60 * 60 * 1000,
-        });
-        const userToken = serialize('user', getSessionCookie, {
-          httpOnly: true,
-          path: '/',
-          expires: new Date(Date.now() + 336 * 60 * 60 * 1000),
-        });
-        res.setHeader('Set-Cookie', userToken);
-        res.status(200).json({ message: 'Successfully set cookie!' });
-      })
-      .catch(() => {
-        res.status(401).json({ message: 'invlalid token' });
+    try {
+      await verifyToken(token);
+      const getSessionCookie = await createCookie(token);
+      const userToken = serialize('user', getSessionCookie, {
+        httpOnly: true,
+        path: '/',
+        expires: new Date(Date.now() + 336 * 60 * 60 * 1000),
       });
+      res.setHeader('Set-Cookie', userToken);
+      res.status(200).json({ message: 'Successfully set cookie!' });
+    } catch (error) {
+      res.status(401).json({ message: 'invlalid token' });
+    }
   }
 };
