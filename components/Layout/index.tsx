58d0e69ef2e399e3d13de9bbbccdbbafed63f1b3
박지윤 <jyun1937@naver.com>
2023-03-14 14:25:31 +0900

    Fix: 레이아웃 컴포넌트 샘플 로그아웃 버튼 삭제

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,22 +1,12 @@
 import { Container } from './style';
-import { authAPI, localAuth } from 'apis/auth';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 export const Layout = ({ children }: React.PropsWithChildren) => {
-  const router = useRouter();
-  const onSingOut = async () => {
-    await authAPI.signout();
-    await localAuth.signout();
-    router.push('/signin');
-  };
-
   return (
     <>
       <Head>
         <title>룰루트레이닝</title>
       </Head>
-      <button onClick={onSingOut}>로그아웃</button>
       <Container>{children}</Container>
     </>
   );
